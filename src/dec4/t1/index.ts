0ef246fee89e00fc3499cfc3a8bf555b15770ee5
Johan <johan.kristiansson@divid.se>
2017-12-04 10:36:25 +0100

    dec 4

---
+++ src/dec4/t1/index.ts
@@ -0,0 +1,4 @@
+export function go(rawInput: string): number {
+    const input = rawInput.split("\n").map(r => r.trim().split(" "));
+    return input.reduce((a, b) => a + (!b.reduce((a2, b2) => a2 + (b.filter(b3 => b3 === b2).length > 1 ? 1 : 0), 0) ? 1 : 0), 0);
+}
