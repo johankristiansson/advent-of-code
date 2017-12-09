f9e4461dbf8e96a9235146b25c7902e6209884a3
johankristiansson <johan1kristiansson@gmail.com>
2017-12-09 21:42:30 +0100

    Refactor to function

--- src/dec9/t1/index.ts
+++ src/dec9/t1/index.ts
@@ -1,26 +1,6 @@
 export function go(rawInput: string): number {
-    let ix = 0;
-    let sum = 0;
-    let deepth = 0;
-    let isGarbage = false;
-    while(ix < rawInput.length) {
-        let char = rawInput.charAt(ix);
-        if(char === "!") {
-            ix++;
-        }
-        else if(char === "{" && !isGarbage){
-            sum += ++deepth;
-        }
-        else if (char === "}" && !isGarbage){
-            deepth--;
-        }
-        else if(char === "<"){
-            isGarbage = true;
-        }
-        else if(char === ">"){
-            isGarbage = false;
-        }
-        ix++;
-    }
-    return sum;
+    const filteredIgnored = rawInput.split("!!").join("").split("!").map((r, ix) => ix === 0 ? r : r.substring(1)).join("");
+    const clean = filteredIgnored.split(">").map((f, ix)=> ~f.indexOf("<") ? f.substring(0, f.indexOf("<")) : f.substring(0)).join("");
+    const finalizedStream = clean.split(",").join("");
+    return finalizedStream.split("").reduce((a, b, ix) => a + (b === "}" ? (finalizedStream.substring(0, ix).split("").filter(f => f === "{").length - finalizedStream.substring(0, ix).split("").filter(f => f === "}").length) : 0), 0);
 }
